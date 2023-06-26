1c3a0300675d8b1bdf94553ec5b9d4a0b6ba8797
Juhwan Jung <remagman@gmail.com>
2023-06-25 21:39:58 -0400

    Many Updates

--- src/App.js
+++ src/App.js
@@ -1,4 +1,11 @@
 import logo from './logo.svg';
+import { 
+  motion,
+  useScroll,
+  useSpring,
+  useTransform,
+  MotionValue
+} from "framer-motion";
 import {BGE} from "./styles";
 import React, { useRef, useState, useContext, useEffect } from "react";
 import './App.css';
@@ -13,10 +20,22 @@ import project1 from './images/project1.png';
 import project2 from './images/project2.png';
 import portrait from './images/portrait.jpg';
 
+const itemVariants: Variants = {
+  open: {
+    opacity: 1,
+    y: 0,
+    transition: { type: "spring", stiffness: 300, damping: 24 }
+  },
+  closed: { opacity: 0, y: 20, transition: { duration: 0.2 } }
+};
+
+
 function App() {
 
 
+  const [isOpen, setIsOpen] = useState(false);
   const [hover, setHover] = useState(true);
+  const [select, setSelect] = useState(false);
 
   const handleMouseEnter = () => {
     setHover(true);
@@ -28,37 +47,78 @@ function App() {
 
 
   return(
-    <div className = "BG">
-      
-      <div className = "Button"> {/* Plus Button */}
-        <img src={plus} width = "80" height = "80"/>
-      </div>
 
-      <div className = "Home">
-        <div className = "Intro">
+    <div className = "BG">
+      <motion.div whileHover = {{scale: 1.2}} whileTap = {{scale: 0.97}} onClick = {() => setIsOpen(!isOpen)} className = "Button"> {/* Plus Button */}
+        <motion.img  animate = {isOpen ? "open" : "closed"} variants = {{open: { rotate: 135}, closed: { rotate: 0 }}} src={plus} width = "80" height = "80"/>
+      </motion.div>
+      <motion.ul class = "Menu" style = {{
+        listStyleType: "none", 
+        position: "fixed", 
+        right: "3vw", 
+        top: "9vh", 
+        display: "flex", 
+        flexDirection: "column",
+        gap:"1vh",
+        fontSize: "2vh"
+        }
+      }
+
+        animate = {
+          isOpen ? "open" : "closed"
+        } 
+        
+        variants={{
+          open: {
+            transition: {
+              type: "spring",
+              bounce: 0,
+              duration: 0.7,
+              delayChildren: 0.3,
+              staggerChildren: 0.05
+            }
+          },
+          closed: {
+            transition: {
+              type: "spring",
+              bounce: 0,
+              duration: 0.2
+              
+            }
+          }
+        }}>
+        <motion.li class = "Parts" whileHover = {{scale: 1.05}} variants={itemVariants}>home</motion.li>
+        <motion.li class = "Parts" whileHover = {{scale: 1.05}} variants={itemVariants}>about</motion.li>
+        <motion.li class = "Parts" whileHover = {{scale: 1.05}} variants={itemVariants}>experience</motion.li>
+        <motion.li class = "Parts" whileHover = {{scale: 1.05}} variants={itemVariants}>project</motion.li>
+      </motion.ul>
+
+
+      <section className = "Home">
+        <motion.div initial = {{ opacity: 0}} whileInView={{opacity: 1}} transition={{}}className = "Intro">
           <header className = "Name">
             Hi, I'm <span style = {{color: "#4200FF"}}>Juhwan.</span> <div className = "Desc">Sometimes I go by J.</div>
             <div className = "BigDesc">I’m a software engineer based in Pennsylvania. <div>I have huge interests in the field of A.I. as well as web development.</div></div>
           </header>
-        </div>
-        <div className = "SBC" style = {{marginLeft: "5vw", marginTop: "18vh"}}>
-          <img src={sendi} width = "50" height = "50"/>
-          <img src={linkedini} width = "50" height = "50"/>
-          <img src={inboxi} width = "50" height = "50"/>
-          <img src={githubi} width = "50" height = "50"/>
-        </div>
-      </div>
+        </motion.div>
+        <motion.div className = "SBC" style = {{marginLeft: "5vw", marginTop: "18vh"}} >
+          <a href = "mailto:jhj5347@gmail.com"><motion.img initial = {{opacity: 0, scale: 0.5}} animate = {{opacity: 1, scale: 1}} transition = {{duration: 1, ease: [0, 0.71, 0.2, 1.01], scale: {type: "spring", damping: 5, stiffness: 100, restDelta: 0.001}}}  whileHover = {{scale: 1.2}} src={sendi} width = "50" height = "50"/></a>
+          <a href = "https://www.linkedin.com/in/juhwan-jung"><motion.img initial = {{opacity: 0, scale: 0.5}} animate = {{opacity: 1, scale: 1}} transition = {{duration: 1, ease: [0, 0.71, 0.2, 1.01], scale: {type: "spring", damping: 5, stiffness: 100, restDelta: 0.001}}}whileHover = {{scale: 1.2}} src={linkedini} width = "50" height = "50"/></a>
+          <motion.img initial = {{opacity: 0, scale: 0.5}} animate = {{opacity: 1, scale: 1}} transition = {{duration: 1, ease: [0, 0.71, 0.2, 1.01], scale: {type: "spring", damping: 5, stiffness: 100, restDelta: 0.001}}}whileHover = {{scale: 1.2}} src={inboxi} width = "50" height = "50"/>
+          <a href = "https://www.github.com/juhwani"><motion.img initial = {{opacity: 0, scale: 0.5}} animate = {{opacity: 1, scale: 1}} transition = {{duration: 1, ease: [0, 0.71, 0.2, 1.01], scale: {type: "spring", damping: 5, stiffness: 100, restDelta: 0.001}}}whileHover = {{scale: 1.2}} src={githubi} width = "50" height = "50"/></a>
+        </motion.div>
+      </section>
 
             {/* Start of About Page */}
-      <div className = "About">
+      <section className = "About">
         <div className = "Sphere">
           <img style = {{marginTop: "-7vh", marginLeft: "-5vw"}} src={portrait} width = "676" height = "885"/>
         </div>
-        <header className = "ZO">01</header>
+        <motion.header  className = "ZO">01</motion.header>
         <div className = "AboutD">
           <header style = {{ fontSize: 55, fontWeight: "bold"}}>ABOUT</header>
           <div className = "AboutP">
-            <p>Originated from South Korea, I am currently studying Bachelor’s of Computer Science at Pennsylvania State University.</p>
+            <p>Originated from South Korea, I am currently studying Bachelor’s of Computer Science at the Pennsylvania State University.</p>
             <p>I have had experiences working at a non-profit medical organization as a front-end developer as well as online mobile game software development company</p>
             <p>I am a developer that believe that A.I. will be the gateway to solutions of many of the problems we are experiencing at the global and local level.</p>
           </div>
@@ -75,10 +135,10 @@ function App() {
             </div>
           </div>
         </div>
-      </div>
+      </section>
 
 {/* Start of Experience */}
-      <div className="Experience">
+      <section className="Experience">
         <div className="ZT">02
           <div className="Exp">
             <header style = {{fontSize: "6vh", fontWeight: "bold", color: "black"}}>EXPERIENCE</header>
@@ -115,15 +175,16 @@ function App() {
             </header>
           </div>
         </div>
-      </div>
+      </section>
 
 {/* Start of Projects */}
 
-      <div className="Projects">
+      <section className="Projects">
         <div className="ZT" style = {{fontSize: 128, marginLeft: "10vw"}}>03</div>
         <header style = {{fontWeight: "bold",fontSize: 64, marginLeft: "16vw"}}>PROJECTS</header>
-      </div>
-      <div className="ProjectsD">
+      </section>
+      
+      <section className="ProjectsD">
         {hover && <div onMouseLeave = {() => {handleMouseLeave();}} className="ProjectsI">
           <header style = {{fontSize : "64px",marginRight: "195px"}}>PROJECT 1</header>
           <p style = {{width: "500px", fontSize: "24px", marginTop: "50px"}}>Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. </p>
@@ -138,34 +199,40 @@ function App() {
           <p style = {{width: "500px", fontSize: "24px", marginTop: "50px"}}>Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. </p>
           <header style = {{fontSize: "24px", marginRight: "380px", marginBottom: "150px"}}>view details</header>
         </div>}
-      </div>
-      <div className="ProjectsD">
-        <div className = "ProjectsE">
-          <img src={folder} width = "50" height = "40"/>
-          <div style = {{fontWeight: "bold", fontSize: "24px", marginTop: "41vh", marginLeft: "2vw"}}>PROJECT 3
-            <p style = {{fontWeight: "normal", width: "25vw"}}>Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum.</p>
-            <p style = {{fontWeight: "bold", marginTop: 60}}>view details</p>
+      </section>
+
+
+      <section className="ProjectsN">
+        <div className="Row">
+          <div className = "ProjectsE">
+            <img src={folder} width = "50" height = "40"/>
+            <div style = {{fontWeight: "bold", fontSize: "24px", marginTop: "41vh", marginLeft: "2vw"}}>PROJECT 3
+              <p style = {{fontWeight: "normal", width: "25vw"}}>Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum.</p>
+              <p style = {{fontWeight: "bold", marginTop: 60}}>view details</p>
+            </div>
+          </div>
+          <div className = "ProjectsE">
+            <img src={folder} width = "50" height = "40"/>
+            <div style = {{fontWeight: "bold", fontSize: "24px", marginTop: "41vh", marginLeft: "2vw"}}>PROJECT 4
+              <p style = {{fontWeight: "normal", width: "25vw"}}>Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum.</p>
+              <p style = {{fontWeight: "bold", marginTop: 60}}>view details</p>
+            </div>
           </div>
         </div>
-        <div className = "ProjectsE">
-          <img src={folder} width = "50" height = "40"/>
-          <div style = {{fontWeight: "bold", fontSize: "24px", marginTop: "41vh", marginLeft: "2vw"}}>PROJECT 4
-            <p style = {{fontWeight: "normal", width: "25vw"}}>Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum.</p>
-            <p style = {{fontWeight: "bold", marginTop: 60}}>view details</p>
-          </div>
-        </div>
-      </div>
 
-      <div className = "SBC" style = {{marginTop: "15vw"}}>
-        <img src={sendi} width = "50" height = "50"/>
-        <img src={linkedini} width = "50" height = "50"/>
-        <img src={inboxi} width = "50" height = "50"/>
-        <img src={githubi} width = "50" height = "50"/>
-      </div>
+        <motion.div className = "SBC" style = {{marginTop: "8vh"}}>
+          <a href = "mailto:jhj5347@gmail.com"><motion.img initial = {{opacity: 0, scale: 0.5}} animate = {{opacity: 1, scale: 1}} transition = {{duration: 1, ease: [0, 0.71, 0.2, 1.01], scale: {type: "spring", damping: 5, stiffness: 100, restDelta: 0.001}}}  whileHover = {{scale: 1.2}} src={sendi} width = "50" height = "50"/></a>
+          <a href = "https://www.linkedin.com/in/juhwan-jung"><motion.img initial = {{opacity: 0, scale: 0.5}} animate = {{opacity: 1, scale: 1}} transition = {{duration: 1, ease: [0, 0.71, 0.2, 1.01], scale: {type: "spring", damping: 5, stiffness: 100, restDelta: 0.001}}}whileHover = {{scale: 1.2}} src={linkedini} width = "50" height = "50"/></a>
+          <motion.img initial = {{opacity: 0, scale: 0.5}} animate = {{opacity: 1, scale: 1}} transition = {{duration: 1, ease: [0, 0.71, 0.2, 1.01], scale: {type: "spring", damping: 5, stiffness: 100, restDelta: 0.001}}}whileHover = {{scale: 1.2}} src={inboxi} width = "50" height = "50"/>
+          <a href = "https://www.github.com/juhwani"><motion.img initial = {{opacity: 0, scale: 0.5}} animate = {{opacity: 1, scale: 1}} transition = {{duration: 1, ease: [0, 0.71, 0.2, 1.01], scale: {type: "spring", damping: 5, stiffness: 100, restDelta: 0.001}}}whileHover = {{scale: 1.2}} src={githubi} width = "50" height = "50"/></a>
+        </motion.div>
 
-      <div className="CopyRight">
-        Built and designed by Juhwan Jung. <header>All rights reserved. ©</header>
-      </div>
+        <div className="CopyRight">
+          Built and designed by Juhwan Jung. <header>All rights reserved. ©</header>
+        </div>  
+      </section>
+
+      
 {/* testing */}
     </div>
   );
